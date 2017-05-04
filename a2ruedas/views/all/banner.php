5de41eed7f4bab3f9dc6db49b6bbbb30d9b5b08c
SENA <SENA@DESKTOP-L0GLE2A>
2017-05-04 12:58:38 -0500

    primero

--- a2ruedas/views/all/banner.php
+++
@@ -1,4 +0,0 @@
-<section style="height: 140px; background-image:url('assets/img/back_login2.png');background-repeat:repeat;background-position:center;"><center><img src="assets/img/logo.png" class="img-responsive" width=""></center>
-</section>
-
-<!-- <div style="overflow: hidden;height: 150px;width: 100%;"><img src="assets/img/back_login2.png"><img src="assets/img/back_login2.png"><img src="assets/img/back_login2.png"><img src="assets/img/back_login2.png"><img src="assets/img/back_login2.png"></div> -->
